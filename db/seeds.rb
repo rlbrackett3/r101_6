280b4beac40cbbad88f8dbc15377fa25f0c51810
Robert Lee Brackett III <robert.brackett3@gmail.com>
2014-12-12 10:29:06 -0500

    Update seeds.rb
    I added some seed stuff here to make it easier to play around, you can run rake db:setup to load this

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#I added some seed stuff here to make it easier to play around, you can run rake db:setup to load this
+puts "Create a test User"
+User.create(fname: "Foo", lname: "Bar", username: "foobar", email: "foo@bar.com", password: "foobar")
+puts "User created successfully"
+
+puts "Create some Boards"
+5.times do |i|
+  Board.create(name: "Board Number #{i}", description: "There are cool things here")
+end
+puts " Boards created successfully"
+
+puts "Create some Topics"
+10.times do |i|
+  Topic.create(title: "Topic #{i}", body: "My random through number #{i}")
+end
+puts " Topics created successfully"
